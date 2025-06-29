d8a17e64772de25ec88d0d630a5ca8d076c2154d
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2025-06-29 13:51:09 +0530

    Added counter functionality to app

--- app/src/main/java/com/anirudh/japatracker/MainActivity.java
+++ app/src/main/java/com/anirudh/japatracker/MainActivity.java
@@ -1,46 +1,69 @@
 package com.anirudh.japatracker;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.anirudh.japatracker.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton login;
+    private Button gayatri_button, raghavendra_buton;
+    private TextView gayatri_text,raghavendra_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton login=(ImageButton) findViewById(R.id.login_fab);
 
-        try{
-            login.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Snackbar.make(v,"Will proceed to login",Snackbar.LENGTH_LONG)
-                            .setAnchorView((R.id.login_fab))
-                            .setAction("Login_Button",null).show();
-                }
-            });
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+        //Login Button
+        login=(ImageButton) findViewById(R.id.login_fab);
+
+        //Counting Buttons
+        gayatri_button =(Button) findViewById(R.id.GayatriMantra);
+        raghavendra_buton =(Button) findViewById(R.id.RaghavendraMantra);
+
+        //Text Views
+        gayatri_text=(TextView) findViewById(R.id.gayatri_text);
+        raghavendra_text=(TextView) findViewById(R.id.raghavendra_text);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Will proceed to login",Snackbar.LENGTH_LONG)
+                        .setAnchorView((R.id.login_fab))
+                        .setAction("Login_Button",null).show();
+            }
+        });
+
+        gayatri_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] info=gayatri_text.getText().toString().split(":");
+                int num=Integer.parseInt(info[1].trim());
+                num++;
+                String message=String.format(Locale.ENGLISH,"%s: %d",info[0],num);
+                gayatri_text.setText(message);
+            }
+        });
+        raghavendra_buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] info=raghavendra_text.getText().toString().split(":");
+                int num=Integer.parseInt(info[1].trim());
+                num++;
+                String message=String.format(Locale.ENGLISH,"%s: %d",info[0],num);
+                raghavendra_text.setText(message);
+            }
+        });
     }
     }
